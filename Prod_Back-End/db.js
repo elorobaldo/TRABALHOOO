c8179e75773938f7caca53ff3c41a5dbbdcba501
elorobaldo <elo.robaldo1@gmail.com>
2023-06-16 22:00:58 -0400

    config do trab

---
+++ Prod_Back-End/db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://cgslbvje:d3fOv_0mAJMWXqbcdt4-4p6vws_bF1v7@silly.db.elephantsql.com/cgslbvje'
+    });              //link elephante(banco)sdjfjsgfh
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
---
+++ Prod_Back-End/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require('path');
+const Prod = require("./models/prod");
+const cors= require('cors');
+const app = express();
+app.use(cors());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/prods', async function(req, res){
+  try {
+    var prods = await Prod.select();
+    res.json(prods.rows);
+  } catch (error) {
+    console.error('Erro ao buscar prods:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao buscar prods' });
+  }
+});
+
+app.post('/prods', async function(req, res){
+  try {
+    var prods = await Prod.selectOne(req.body.id);
+    res.json(prods.rows[0]);
+  } catch (error) {
+    console.error('Erro ao buscar prods:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao buscar prods' });
+  }
+});
+
+app.put('/prods', async function(req, res){
+  try {
+    var prods = await Prod.update(req.body.id, req.body);
+    res.json(prods.rows);
+  } catch (error) {
+    console.error('Erro ao atualizar produto:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao atualizar produto' });
+  }
+});
+
+app.delete('/prods', async function(req, res){
+  try {
+    var prod = await Prod.delete(req.body.id);
+    res.json(prod.rows);
+  } catch (error) {
+    console.error('Erro ao atualizar prod:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao atualizar prod' });
+  }
+});
+
+
+app.listen(3001, function() {
+  console.log(`app de Exemplo escutando na porta! ${3001}`)
+});
